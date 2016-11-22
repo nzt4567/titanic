b30ac53f07c3f73be1d859171c342e230ceefdeb
Tomas Kvasnicka <tomas.kvasnicka@cdn77.com>
2016-11-22 23:30:23 +0100

    finish
    - choose elements using indexers
    - finish last task

--- titanic.py
+++ titanic.py
@@ -12,6 +12,10 @@ def main():
                   5: '50-59',
                   6: '60-69',
                   7: '70-79'}
+    pclass_map = {'*': 0,
+                  '1st': 1,
+                  '2nd': 2,
+                  '3rd': 3}
 
     d = pandas.read_csv('data.csv')
     num_passengers = len(d)
@@ -26,13 +30,13 @@ def main():
     print("Overall survivors:", str(num_survivors / num_passengers * 100) + " %\n", sep='\n')
 
     # 3.
-    sex = d[['Survived', 'SexCode']].groupby('SexCode').sum()
+    sex = d.loc[:, ['Survived', 'SexCode']].groupby('SexCode').sum()
     print("Male survivors:", str(sex.loc[0, 'Survived'] / num_passengers * 100) + " %", sep='\n')
     print("Female survivors:", str(sex.loc[1, 'Survived'] / num_passengers * 100) + " %\n", sep='\n')
 
     # 4.
-    survivors_by_class = d[['PClass', 'Survived']].groupby('PClass').sum()
-    passengers_by_class = d[['PClass', 'Survived']].groupby('PClass').count()
+    survivors_by_class = d.loc[:, ['PClass', 'Survived']].groupby('PClass').sum()
+    passengers_by_class = d.loc[:, ['PClass', 'Survived']].groupby('PClass').count()
 
     print("Class survival statistics:")
     for c in survivors_by_class.index:
@@ -41,16 +45,30 @@ def main():
         print(s, 'out of', p, 'from', c if c != '*' else 'unknown', 'class survived -', s / p * 100, '%')
 
     # 5.
-    survivors_by_decade = d[['Decade', 'Survived']].fillna('unknown').groupby('Decade').sum() / num_survivors * 100
-    survivors_by_decade.plot(kind='bar', figsize=(10, 10)).set_ylabel('Percentage')
+    survivors_by_decade = d.loc[:, ['Decade', 'Survived']].fillna('unknown'). \
+                              groupby('Decade').sum() / num_survivors * 100
+    survivors_by_decade.plot(kind='bar', figsize=(10, 10), title='Percentage of survivors based on their age'). \
+        set_ylabel('Percentage')
     matplotlib.pyplot.savefig('decades.png')
 
     # 6.
+    d.loc[:, ['Age', 'PClass']].groupby('PClass').mean().dropna(). \
+        plot(kind='bar', figsize=(10, 10), legend=False, title='Mean age based on class'). \
+        set_ylabel('Age')
+    matplotlib.pyplot.savefig('mean.png')
+    d.loc[:, ['Age', 'PClass']].groupby('PClass').max().dropna(). \
+        plot(kind='bar', figsize=(10, 10), legend=False, title='Maximal age based on class'). \
+        set_ylabel('Age')
+    matplotlib.pyplot.savefig('max.png')
+    d.loc[:, ['Age', 'PClass']].groupby('PClass').min().dropna(). \
+        plot(kind='bar', figsize=(10, 10), legend=False, title='Minimal age based on class'). \
+        set_ylabel('Age')
+    matplotlib.pyplot.savefig('min.png')
 
-
-    # print(survived)
-    # print(len(survived))
-    # print(d.info())
+    d.loc[:, 'PClass'] = d.loc[:, 'PClass'].dropna().apply(lambda x: pclass_map[x])
+    d.loc[:, ['Age', 'PClass']].dropna().plot.hexbin(x='PClass', y='Age', gridsize=17,
+                                                     title='Age distribution based on class')
+    matplotlib.pyplot.savefig('age_class.png')
 
 
 if __name__ == "__main__":
